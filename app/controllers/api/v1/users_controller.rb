bb300e65b460409245865bf773a1bbd3c4562e16
stephenkati <stephenkatuli88@gmail.com>
2023-07-25 03:31:59 +0300

    Setup users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      token = encoded_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { message: 'Invalid credentials' }
+    end
+  end
+
+  def login
+    @user = User.find_by(username: login_params[:username])
+
+    if @user&.authenticate(login_params[:password])
+      token = encoded_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { message: 'Invalid credentials' }
+    end
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
